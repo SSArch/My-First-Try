68f34fd68dccdfd06aca3c5183e9707df1318407
SSArch <arslansinansadk21@gmail.com>
2023-01-20 12:50:35 +0300

    First Commit

---
+++ CubeSurfers Clone/Assets/Kodlar/Toplayici.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toplayici : MonoBehaviour
+{
+    GameObject anaKup;
+    int yukseklik;
+    void Start()
+    {
+        anaKup = GameObject.Find("AnaKüp");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anaKup.transform.position = new Vector3(transform.position.x, yukseklik + 1, transform.position.z);
+        this.transform.localPosition = new Vector3(0, -yukseklik, 0);
+    }
+    public void YukseklikAzalt()
+    {
+        yukseklik--;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Topla"&&other.gameObject.GetComponent<ToplanabilirKup>().GetToplandiMi()==false)
+        {
+            yukseklik += 1;
+            other.gameObject.GetComponent<ToplanabilirKup>().ToplandiYap();
+            other.gameObject.GetComponent<ToplanabilirKup>().IndexAyarla(yukseklik);
+            other.gameObject.transform.parent = anaKup.transform;
+        }
+    }
+}
